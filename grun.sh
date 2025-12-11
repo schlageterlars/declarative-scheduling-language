d4f35d66a56fbd6bd80474204da97c3ce3a84b0a
Lars <schlageterlars@gmail.com>
2025-12-11 18:30:11 +0100

    fix: org.antlr.v4.gui.TestRig

--- grun.sh
+++ grun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gui="java -Djava.awt.headless=false -cp DSL.jar org.antlr.v4.gui.TestRig DSL schedule -gui $1"
+gui="java -Djava.awt.headless=false -cp target/DSL.jar org.antlr.v4.gui.TestRig generated.DSL schedule -gui $1"
 echo $gui
 eval $gui
